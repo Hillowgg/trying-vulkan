9aa034ebed24fa06a08b7cadc0c3c732fd22fb50
hillow <andrew.hillow@gmail.com>
2023-01-30 23:37:09 +0300

    init commit

---
+++ window.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <string>
+namespace lve {
+    class Window {
+        public:
+            Window(int w, int h, std::string name);
+            ~Window();
+
+            bool shouldClose() { return glfwWindowShouldClose(window); }
+
+        private:
+            void initWindow();
+
+            const int width;
+            const int height;
+
+            std::string windowName;
+            GLFWwindow *window;
+    };
+}
